77fb809374dd852e50653ca6d07320a37cc27128
u03013112 <u03013112@hotmail.com>
2021-12-17 17:14:03 +0800

    build bgc

--- js/inject.js
+++ js/inject.js
@@ -7,34 +7,25 @@ function test() {
     parentNode.insertBefore(newNode,canvas);
 }
 
-function timerTest() {
-    if (window.j_timer != null) {
-        clearInterval(window.j_timer);
-        window.j_timer = null;
-    }else{
-        window.j_timer = setInterval(function() {console.log('1');},1000);
-    }
-}
-
 // 获取兵营信息
 function THGetBGCinfos() {
     ret = [];
-    bbIdList = [1041, 1042, 1043, 1044, 1045, 1046, 1047, 1048, 1049, 1050, 104011, 104012, 104013, 104014, 104015, 104016, 104017, 104018, 104019, 104020, 104021, 104022, 104023, 104024, 104025, 104026, 104027, 104028, 104029, 104030, 104031, 104032, 104033, 104034, 104035, 104036, 104037, 104038, 104039, 104040, 104041, 104042, 104043, 104044, 104045, 104046, 104047, 104048, 104049, 104050, 104051, 104052, 104053, 104054, 104055, 104056, 104057, 104058, 104059, 104060, 104061, 104062, 104063, 104064, 104065, 104066, 104067, 104068, 104069, 104070, 104071, 104072, 104073, 104074, 104075, 104076, 104077, 104078, 104079, 104080, 104081, 104082, 104083, 104084, 104085, 104086, 104087, 104088, 104089, 104090, 104091, 104092, 104093, 104094, 104095, 104096, 104097, 104098, 104099, 104100]
-    for(var i = 0; i <bbIdList.length;++i){
+    // bbIdList = [1041, 1042, 1043, 1044, 1045, 1046, 1047, 1048, 1049, 1050, 104011, 104012, 104013, 104014, 104015, 104016, 104017, 104018, 104019, 104020, 104021, 104022, 104023, 104024, 104025, 104026, 104027, 104028, 104029, 104030, 104031, 104032, 104033, 104034, 104035, 104036, 104037, 104038, 104039, 104040, 104041, 104042, 104043, 104044, 104045, 104046, 104047, 104048, 104049, 104050, 104051, 104052, 104053, 104054, 104055, 104056, 104057, 104058, 104059, 104060, 104061, 104062, 104063, 104064, 104065, 104066, 104067, 104068, 104069, 104070, 104071, 104072, 104073, 104074, 104075, 104076, 104077, 104078, 104079, 104080, 104081, 104082, 104083, 104084, 104085, 104086, 104087, 104088, 104089, 104090, 104091, 104092, 104093, 104094, 104095, 104096, 104097, 104098, 104099, 104100]
+    for(var i = 0; i <THBGCIdList.length;++i){
         ret[i] = 0;
     }
     bList = cc.find('Canvas/HomeMap/BuildingNode').getChildren();
     for (var i = 0; i < bList.length; ++i) {
         b = bList[i]; if (b.name == 'BuildingItem') {
             c = b.getComponent('BuildingItem');
-            index = bbIdList.indexOf(c.ItemData.id);
+            index = THBGCIdList.indexOf(c.ItemData.id);
             if (index<0){
                 continue;
             }
             ret[index] += 1;
         }
     }
-    console.log(ret)
+    // console.log(ret)
     return ret;
 }
 function getArmyInfos() {
@@ -51,8 +42,8 @@ function getArmyInfos() {
 function buildBGC() {
     // BuildNewBuilding
     c = cc.find('Canvas/HomeMap').getComponent('HomeMap');
-    c.BuildNewBuilding(104019,-1,true)
-    c.BuildNewBuilding(104019,-1,true)
+    c.BuildNewBuilding(104020,-1,true)
+    c.BuildNewBuilding(104020,-1,true)
     if (c.AddingItem) {
         c.CancelBuildBuilding(c.AddingItem);
     }
@@ -62,7 +53,7 @@ function buildBGC() {
 function THDataInit() {
     if (window.THData == null) {
         window.THData = {}
-        window.THData.timer = setInterval(function() {console.log('1');},1000);
+        window.THData.timer = setInterval(function() {THTaskUpdate();},1000);
     }
 }
 
@@ -109,6 +100,19 @@ function THGetKJ() {
     window.THData.KJLevel = levels;
 }
 
+// 任务调度器
+function THTaskUpdate() {
+    if (window.THData.Tasks && window.THData.Tasks.length>0){
+        for (var i=0;i<window.THData.Tasks.length;++i){
+            task = window.THData.Tasks[i];
+            // TODO:判断整体状态，比如界面不对的时候要暂停任务
+            if (task.type == 'BGCBuild'){
+                THBGCBuildTask(task);
+            }
+        }
+    }
+}
+
 function taskMainBtnClicked() {
     taskMainButton = document.getElementById("topwar_helper_taskMainButton");
     if (window.THData.timer != null) {
@@ -116,7 +120,7 @@ function taskMainBtnClicked() {
         window.THData.timer = null;
         taskMainButton.innerHTML = '任务暂停中';
     }else{
-        window.THData.timer = setInterval(function() {console.log('1');},1000);
+        window.THData.timer = setInterval(function() {THTaskUpdate();},1000);
         taskMainButton.innerHTML = '任务进行中';
     }
 }
@@ -206,6 +210,11 @@ function THBGCDivInit() {
     BGCDiv.style.background='pink';
     parentNode.append(BGCDiv);
     
+    var BGCTitle = document.createElement("h3");
+    BGCTitle.style.margin='8px';
+    BGCTitle.innerHTML = '兵工厂';
+    BGCDiv.append(BGCTitle);
+
     // 科技
     {
         if (window.THData.KJLevel == null) {
@@ -274,7 +283,7 @@ function THBGCDivInit() {
         var BGCTaskDiv = document.createElement("div");
         BGCTaskDiv.id = 'topwar_helper_BGCTaskDiv';
         BGCTaskDiv.style.width='200px';
-        BGCTaskDiv.style.height='200px';
+        BGCTaskDiv.style.height='100px';
         BGCTaskDiv.style.background='white';
         BGCDiv.append(BGCTaskDiv);
 
@@ -299,5 +308,42 @@ function THBGCDivInit() {
         BGCTaskButton.innerHTML = '下发任务';
         BGCTaskButton.setAttribute("onclick", "BGCTaskButtonClicked()");
         BGCTaskDiv.append(BGCTaskButton);        
+    }
+}
+
+// 兵营Id列表，找策划要的
+const THBGCIdList = [1041, 1042, 1043, 1044, 1045, 1046, 1047, 1048, 1049, 1050, 104011, 104012, 104013, 104014, 104015, 104016, 104017, 104018, 104019, 104020, 104021, 104022, 104023, 104024, 104025, 104026, 104027, 104028, 104029, 104030, 104031, 104032, 104033, 104034, 104035, 104036, 104037, 104038, 104039, 104040, 104041, 104042, 104043, 104044, 104045, 104046, 104047, 104048, 104049, 104050, 104051, 104052, 104053, 104054, 104055, 104056, 104057, 104058, 104059, 104060, 104061, 104062, 104063, 104064, 104065, 104066, 104067, 104068, 104069, 104070, 104071, 104072, 104073, 104074, 104075, 104076, 104077, 104078, 104079, 104080, 104081, 104082, 104083, 104084, 104085, 104086, 104087, 104088, 104089, 104090, 104091, 104092, 104093, 104094, 104095, 104096, 104097, 104098, 104099, 104100];
+
+// 处理建造兵工厂的任务
+function THBGCBuildTask(task) {
+    homeMap = cc.find('Canvas/HomeMap').getComponent('HomeMap');
+    // 如果目前有多余的正准备添加建筑，就取消掉
+    if (c.AddingItem) {
+        c.CancelBuildBuilding(c.AddingItem);
+    }
+    // 如果已有足够多的兵工厂，任务结束
+    bgcInfos = THGetBGCinfos();
+    if (bgcInfos[task.level-1]>= task.count){
+        console.log("任务完成");
+        for (var i = 0; i <window.THData.Tasks.length;++i){
+            task = window.THData.Tasks[i];
+            if (task.type == 'BGCBuild') {
+                window.THData.Tasks.splice(i, 1);
+                break;
+            }
+        }
+        return
+    }
+    
+    var canMerge = false;
+    
+    if (canMerge) {
+        // 如果有可以合成的兵工厂，就合成
+    }else {
+        // 不能合成就要建造一个目前可以建造的兵工厂
+        // 其实目前可以调接口建更低级的，但是暂时不考虑等级比目前建造等级还低的，没需求
+        var buidingId =  THBGCIdList[task.level-1];
+        homeMap.BuildNewBuilding(104020,-1,true);
+        homeMap.BuildNewBuilding(104020,-1,true);
     }
 }
