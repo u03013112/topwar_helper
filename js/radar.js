b25da3307221a2ced96f9facbc3aac938dcc70fd
u03013112 <u03013112@hotmail.com>
2022-06-15 11:56:42 +0800

    bug

--- js/radar.js
+++ js/radar.js
@@ -545,6 +545,6 @@ function THRadarUpdate() {
         var RadarController = window.__require('RadarController');
         let radarData = RadarController.RadarController.Instance.getData();
         let delta = radarData.endTime - dataCenter.DATA.ServerTime;
-        radar.newMessionTime = Math.floor(delta / 360) + ':' + Math.floor(delta / 60) + ':' + delta % 60
+        radar.newMessionTime = Math.floor(delta / 3600) + ':' + Math.floor(delta / 60) + ':' + delta % 60
     }
 }
